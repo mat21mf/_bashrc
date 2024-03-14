2de232859e8aa95f01fb4c686640b1a4211854d9
Matias Felipe Rebolledo Gonzalez <mfrebolledog@ine.gob.cl>
2024-03-14 12:40:11 -0300

    Edit commandargs parquet.

--- .bin/XlsxAParquet.R
+++ .bin/XlsxAParquet.R
@@ -1,5 +1,10 @@
 #!/usr/bin/env Rscript
 
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("Al menos un argumento debe ser suministrado.\n", call.=FALSE)
+}
+
 suppressPackageStartupMessages({
   library(arrow)
   library(data.table)
@@ -7,19 +12,30 @@ suppressPackageStartupMessages({
 })
 
 XlsxAParquet <- function( strxls , strsht , strout ) {
+
+  ### echo
+  print(paste0(rep("#", 200), collapse = ""))
+  print(paste("### Convirtiendo:"))
+  print(paste("###", strxls))
+  print(paste("### En:"))
+  print(paste("###", strout))
+  print(paste0(rep("#", 200), collapse = ""))
+  cat('\n')
+
+  # cargar
   if( strsht == "" ) {
     objdfm <- setDT( read.xlsx( strxls ) )
   } else {
     objdfm <- setDT( read.xlsx( strxls , sheet = strsht ) )
   }
   write_parquet( objdfm , strout , compression = "brotli" , compression_level = 11 )
+
 }
 
 if (!interactive()) {
-  args <- commandArgs(trailingOnly = TRUE)
   objdfm <- XlsxAParquet(
-    strxls = args[0] ,
-    strsht = args[1] ,
-    strout = args[2]
+    strxls = args[1] ,
+    strsht = args[2] ,
+    strout = args[3]
   )
 }
